ad2aff7ee67b878a760ffab794b6eed33841168a
Ivan <adamovskij00@mail.ru>
2020-08-29 17:54:38 +0300

    Подготавливает проект к защите v0.3

--- source/js/example-range.js
+++ source/js/example-range.js
@@ -27,8 +27,8 @@ function drags(dragElement, exampleCatElement, container) {
       containerOffset = container.offset().left,
       containerWidth = container.outerWidth();
 
-      minLeft = containerOffset + 10;
-      maxLeft = containerOffset + containerWidth - dragWidth - 10;
+      minLeft = containerOffset + 110;
+      maxLeft = containerOffset + containerWidth - dragWidth - 150;
 
   dragElement.parents().on("mousemove touchmove", function(e) {
 
