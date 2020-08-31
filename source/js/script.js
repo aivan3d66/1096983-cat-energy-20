2e6e9daf50133d0c6cab3e5b48b3e9ad9791e8c4
Ivan <adamovskij00@mail.ru>
2020-08-31 10:24:27 +0300

    Очередная финальная ветка

--- source/js/script.js
+++ source/js/script.js
@@ -1,54 +1,48 @@
-var navMain = document.querySelector('.main-nav__list');
-var navToggle = document.querySelector('.hamburger');
-var inputBorder = document.querySelector('.program-form__input');
 var catName = document.querySelector('.input-name');
 var userMail = document.querySelector('.input-mail');
 var userPhone = document.querySelector('.input-phone');
 var mailIcon = document.querySelector('.mail-icon');
 var phoneIcon = document.querySelector('.phone-icon');
+var grayBtn = document.querySelector('.modal--btn');
 
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
-
-userMail.addEventListener("input", function (event) {
+userMail.addEventListener('input', function () {
+  'use strict';
   if (userMail.validity.typeMismatch) {
-    userMail.setCustomValidity("Почта указана неверно");
-    userMail.style.borderColor = "red";
-    mailIcon.style.fill = "red";
+    userMail.setCustomValidity('Почта указана неверно');
+    userMail.style.borderColor = 'red';
+    mailIcon.style.fill = 'red';
   } else {
-    userMail.setCustomValidity("");
-    userMail.style.borderColor = "#68B738";
-    mailIcon.style.fill = "#68B738";
+    userMail.setCustomValidity('');
+    userMail.style.borderColor = '#68B738';
+    mailIcon.style.fill = '#68B738';
   }
 });
 
-userPhone.addEventListener("input", function (event) {
+userPhone.addEventListener('input', function () {
+  'use strict';
   if (userPhone.validity.patternMismatch) {
-    userPhone.setCustomValidity("Номер указан некорректно");
-    userPhone.style.borderColor = "red";
-    phoneIcon.style.fill = "red";
+    userPhone.setCustomValidity('Номер указан некорректно');
+    userPhone.style.borderColor = 'red';
+    phoneIcon.style.fill = 'red';
   } else {
-    userPhone.setCustomValidity("");
-    userPhone.style.borderColor = "#68B738";
-    phoneIcon.style.fill = "#68B738";
+    userPhone.setCustomValidity('');
+    userPhone.style.borderColor = '#68B738';
+    phoneIcon.style.fill = '#68B738';
   }
 });
 
-catName.addEventListener("input", function (event) {
-  if (catName.validity.patternMismatch ) {
-    catName.setCustomValidity("Только кириллица");
-    catName.style.borderColor = "red";
+catName.addEventListener('input', function () {
+  'use strict';
+  if (catName.validity.patternMismatch) {
+    catName.setCustomValidity('Только кириллица');
+    catName.style.borderColor = 'red';
   } else {
-    catName.setCustomValidity("");
-    catName.style.borderColor = "#68B738";
+    catName.setCustomValidity('');
+    catName.style.borderColor = '#68B738';
   }
 });
+
+grayBtn.addEventListener('click', function (event) {
+  'use strict';
+  event.preventDefault();
+}, false);
